c86b0a3a414ced1dcaaaf6ce89d5cc94cb83ef36
Shantanu Godbole <shantanugodbole2000@gmail.com>
2023-06-11 11:37:12 +0530

    syntax issue resolved

--- database.py
+++ database.py
@@ -3,8 +3,8 @@ from supabase import create_client, Client
 
 
 def connectSupabase():
-    url = os.environment["SUPABASE_URL"]
-    key = os.environment["SUPABASE_ID"]
+    url = os.environ["SUPABASE_URL"]
+    key = os.environ["SUPABASE_ID"]
     Client = create_client(url, key)
     return Client
 

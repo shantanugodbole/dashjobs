8b15ff4ed1c7b005de8e83eaa8e9a2ef63882ce1
Shantanu Godbole <shantanugodbole2000@gmail.com>
2023-07-20 22:53:18 +0530

    creating react based frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { userSignIn, userSignUp } from './utils/supabase';
+import {Link} from "react-router-dom";
+import signUp from './screens/signup';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Welcome to DashJobs</h1>
+      <p>A one stop solution to finding opportunities and applying to them</p>
+      <br></br>
+      <h3>Click here to Login to the application</h3>
+      <button onClick={() => userSignIn("shangodwork@gmail.com", "dummypassword")} >Login</button>
+      <br></br>
+      <h4>New to the app? Sign up here!</h4>
+      <Link to="/signup">
+      <button onClick={() => userSignUp("shangodwork@gmail.com", "dummypassword")}>Sign Up</button>
+      </Link>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/signup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SignUp(){
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+
+
+export default SignUp;

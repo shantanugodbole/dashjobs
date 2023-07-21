a3aab0744d710ca5d02a05fb4f48bc84bda90192
Shantanu Godbole <shantanugodbole2000@gmail.com>
2023-07-21 21:44:09 +0530

    removing SignUp page from commit tree

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import SignUp from './screens/SignUp';
 import JobListings from './screens/JobListings';
 import {
   createBrowserRouter,
